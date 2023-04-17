541e75718e7a3b2f0f57458b2cc5c6dbe919806b
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2023-04-17 04:14:39 -0700

    Updated Person Recognition

--- person_recognition/HelloWorldFunction/src/main/java/helloworld/App.java
+++ person_recognition/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,35 +1,19 @@
 package helloworld;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
 
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
-import software.amazon.awssdk.services.rekognition.model.*;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<SQSEvent, String> {
+public class App implements RequestHandler<SQSEvent, Void> {
 
     private LambdaLogger logger;
     private S3Client s3Client;
@@ -37,22 +21,23 @@ public class App implements RequestHandler<SQSEvent, String> {
     private SqsClient sqsClient;
 
     private String bucketName;
-    private String queueURL;
+    private String car_queueURL;
+    private String people_queueURL;
 
 
-    private Void initialize_services(Context context) {
+    private void initialize_services(Context context) {
 
-        logger = context.getLogger();
-
-        return null;
-
+        this.logger = context.getLogger();
+        this.s3Client = S3Client.builder().build();
+        this.rekognitionClient = RekognitionClient.builder().build();
+        this.sqsClient = SqsClient.builder().build();
 
     }
 
     @Override
-    public String handleRequest(SQSEvent sqsEvent, Context context) {
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
 
-        LambdaLogger logger = context.getLogger();
+        initialize_services(context);
 
         for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
             logger.log(message.getBody());
@@ -60,4 +45,6 @@ public class App implements RequestHandler<SQSEvent, String> {
 
         return null;
     }
+
+    /* Make function to check if person is detected. If so enqueue message to new queue. If message is a stop signal, break out of the while-loop */
 }
